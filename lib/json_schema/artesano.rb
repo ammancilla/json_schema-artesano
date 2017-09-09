ab28b70f96a0cc78c4b59cc0196c049550465380
Alfonso Mancilla <almancill@gmail.com>
2017-09-09 18:58:46 +0200

    First commit 🎉

---
+++ lib/json_schema/artesano.rb
@@ -0,0 +1,7 @@
+require "json_schema/artesano/version"
+
+module JsonSchema
+  module Artesano
+    # Your code goes here...
+  end
+end
---
+++ lib/json_schema/artesano/version.rb
@@ -0,0 +1,5 @@
+module JsonSchema
+  module Artesano
+    VERSION = "0.1.0"
+  end
+end
