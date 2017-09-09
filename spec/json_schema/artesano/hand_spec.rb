b90dd7f542468c2605dd03a4e2a364aa9bdd57f9
Alfonso Mancilla <almancill@gmail.com>
2017-09-09 22:06:16 +0200

    Replace #sketch by new #support_sketch helper method

--- spec/json_schema/artesano/hand_spec.rb
+++ spec/json_schema/artesano/hand_spec.rb
@@ -5,12 +5,8 @@ RSpec.describe JsonSchema::Artesano::Hand do
 
   describe '#mold' do
     it 'works' do
-      expect { hand.mold( sketch('schema') ) }.not_to raise_error
+      expect { hand.mold( support_sketch('schema') ) }.not_to raise_error
     end
   end
 end
 
-def sketch(name)
-  schema_path = File.expand_path("../../../support/#{name}.json", __FILE__)
-  JSON.parse( File.read(schema_path) )
-end
