48f0263aae7145bedc5e1d2016b670ad94fbca0e
Alfonso Mancilla <almancill@gmail.com>
2017-09-09 22:06:35 +0200

    Add test case for current existing strategies

--- spec/json_schema/artesano_spec.rb
+++ spec/json_schema/artesano_spec.rb
@@ -1,4 +1,33 @@
 require 'spec_helper'
 
 RSpec.describe JsonSchema::Artesano do
+  let(:sketch) { support_sketch('schema') }
+
+  let(:expected_null) do
+    {"@type"=>nil, "id"=>nil, "uuid"=>nil, "@poll_type"=>nil, "title"=>nil, "image"=>{"@type"=>nil, "cloudinary_id"=>nil, "caption"=>nil, "height"=>nil, "width"=>nil, "original_format"=>nil}, "answers"=>[{"id"=>nil, "uuid"=>nil, "title"=>nil, "correct"=>nil, "un_enum"=>nil}]}
+  end
+
+  let(:expected_data_type) do
+    {"@type"=>"enum[string]", "id"=>"integer", "uuid"=>"string", "@poll_type"=>"enum[string]", "title"=>"string", "image"=>{"@type"=>"enum[string]", "cloudinary_id"=>"string", "caption"=>"string", "height"=>"integer", "width"=>"integer", "original_format"=>"enum[string]"}, "answers"=>[{"id"=>"integer", "uuid"=>"string", "title"=>"string", "correct"=>"boolean", "un_enum"=>"enum"}]}
+  end
+
+  subject { JsonSchema::Artesano::Hand.new(tool: tool).mold(sketch) }
+
+  describe 'tools' do
+    context 'Tools::Null' do
+      let(:tool) { JsonSchema::Artesano::Tools::Null }
+
+      it 'works' do
+	expect { subject }.not_to raise_error
+      end
+    end
+
+    context 'Tools::DataType' do
+      let(:tool) { JsonSchema::Artesano::Tools::DataType }
+
+      it 'works' do
+	expect { subject }.not_to raise_error
+      end
+    end
+  end
 end
